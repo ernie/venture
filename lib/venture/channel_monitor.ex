d215a40014043001b98f36df84f0efb1322d2ea7
Ernie Miller <ernie@ernie.io>
2016-04-07 10:22:39 -0400

    Monitor instead of link

--- lib/venture/channel_monitor.ex
+++ lib/venture/channel_monitor.ex
@@ -18,28 +18,28 @@ defmodule Venture.ChannelMonitor do
   end
 
   def init(:ok) do
-    Process.flag(:trap_exit, true)
     {:ok, %{channels: %{}}}
   end
 
   def handle_call({:monitor, pid, mfa}, _from, state) do
-    Process.link(pid)
-    {:reply, :ok, put_channel(state, pid, mfa)}
+    ref = Process.monitor(pid)
+    {:reply, :ok, put_channel(state, pid, {ref, mfa})}
   end
 
   def handle_call({:demonitor, pid}, _from, state) do
     case Map.fetch(state.channels, pid) do
       :error      -> {:reply, :ok, state}
-      {:ok, _mfa} ->
-        Process.unlink(pid)
+      {:ok, {ref, _mfa}} ->
+        Process.demonitor(ref)
         {:reply, :ok, drop_channel(state, pid)}
     end
   end
 
-  def handle_info({:EXIT, pid, _reason}, state) do
+  def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+    Process.demonitor(ref)
     case Map.fetch(state.channels, pid) do
       :error                   -> {:noreply, state}
-      {:ok, {mod, func, args}} ->
+      {:ok, {_ref, {mod, func, args}}} ->
         Task.Supervisor.start_child(Venture.TaskSupervisor, mod, func, args)
         {:noreply, drop_channel(state, pid)}
     end
@@ -49,8 +49,8 @@ defmodule Venture.ChannelMonitor do
     %{state | channels: Map.delete(state.channels, pid)}
   end
 
-  defp put_channel(state, pid, mfa) do
-    %{state | channels: Map.put(state.channels, pid, mfa)}
+  defp put_channel(state, pid, {ref, mfa}) do
+    %{state | channels: Map.put(state.channels, pid, {ref, mfa})}
   end
 
 end
